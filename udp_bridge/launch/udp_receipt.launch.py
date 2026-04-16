b8ed52e97cdb4c0be6304496fb2b12f4c77b96e3
SantiagoPrice <santiago@ai.iit.tsukuba.ac.jp>
2026-04-16 08:17:32 +0000

    Add launch param

--- udp_bridge/launch/udp_receipt.launch.py
+++ udp_bridge/launch/udp_receipt.launch.py
@@ -1,21 +1,28 @@
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration , PythonExpression
 from launch_ros.actions import Node
 
 
+
 def generate_launch_description():
+
+    log_level = LaunchConfiguration('log_level', default='info')
+    isolated  = PythonExpression(["'",LaunchConfiguration('isolated', default='false'), "' == 'true'"])
+
     return LaunchDescription([
         Node(
             package='udp_bridge',
             executable='udp_manager_node',
             name='udp_manager_node',
             output='log',
-            arguments=['--ros-args', '--log-level', 'warn']
+            parameters=[{'isolated':isolated}],
+            arguments=['--ros-args', '--log-level', log_level]
         ),
         Node(
             package='udp_bridge',
             executable='udp_receiver_node',
             name='udp_receiver_node',
             output='screen',
-            arguments=['--ros-args', '--log-level', 'info']
+            arguments=['--ros-args', '--log-level', log_level]
         ),
     ])
